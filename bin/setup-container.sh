056479186bf549720711f15d95f480cb9ab3bc76
argon-dev22 <hyouga.programing@gmail.com>
2025-07-07 02:45:23 +0000

    devops: project setup more easily

---
+++ bin/setup-container.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# ユーザーからの入力を受け付ける
+echo "Githubのユーザー名を入力してください:"
+read gh_user_name
+
+echo ""
+
+echo "Githubのメールアドレスを入力してください:"
+read gh_email
+
+echo ""
+
+echo "--------------------------------"
+echo "設定内容:"
+echo "Githubのユーザー名: $gh_user_name"
+echo "Githubのメールアドレス: $gh_email"
+echo "--------------------------------"
+echo ""
+
+# 確認のための入力
+echo "上記の設定で続行しますか？ (y/n):"
+read confirmation
+
+echo ""
+
+if [[ $confirmation == "y" || $confirmation == "Y" ]]; then
+    echo "プロジェクトのセットアップを開始します..."
+    cp ./.devcontainer/devcontainer.example.json ./.devcontainer/devcontainer.json
+
+    sed -i "s/Your Name/$gh_user_name/g" ./.devcontainer/devcontainer.json
+    sed -i "s/your.email@example.com/$gh_email/g" ./.devcontainer/devcontainer.json
+
+    cp ./docker-compose.example.yml ./docker-compose.yml
+
+    echo "プロジェクトのセットアップが完了しました。"
+
+    echo ""
+    echo "Dev Containerを起動します..."
+    # devcontainer up
+else
+    echo "キャンセルされました。"
+    exit 1
+fi
